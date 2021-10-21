cf1998ec290ee6cb0d0247c12063a135c022698b
Alex <alexfstos@gmail.com>
2021-10-20 21:27:17 -0300

    finished web app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
 import styles from './App.module.scss'
 import { LoginBox } from './components/LoginBox'
+import { useAuth } from './components/LoginBox/auth'
 import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm'
 
 export function App() {
+  const { user } = useAuth()
   return (
-    <main className={styles.contentWrapper}>
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
       <MessageList/>
-      <LoginBox/>
+      {!!user ? <SendMessageForm/> : <LoginBox/>}
     </main>
   )
 }
--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,11 +1,14 @@
 import {VscGithubInverted} from 'react-icons/vsc'
+import { useAuth } from './auth'
 import styles from './styles.module.scss'
 
 export function LoginBox(){
+    const {signInUrl} = useAuth()
+
     return(
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size={24} />
                 Entrar com github
             </a>
